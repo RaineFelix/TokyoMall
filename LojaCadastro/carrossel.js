7b17b4e7629e0767f5361c07f5bc2e91abab499d
Raíne Felix <103448674+RaineFelix@users.noreply.github.com>
2022-05-06 20:43:04 -0300

    Arquivos Iniciais Adicionados

---
+++ LojaCadastro/carrossel.js
@@ -0,0 +1,28 @@
+const imagensCarrossel = [
+'https://flamboyant.com.br/blog/wp-content/uploads/2020/08/074E474F-BAD3-4A87-98B0-F67CE73CF7D4.jpeg',
+'https://www.falandodeviagem.com.br/imagens19/ViaParqueKids2.jpg',
+'https://invexo.com.br/blog/wp-content/uploads/2020/07/lojas-do-shopping-leblon-1.jpg'
+
+]
+// selecionando o elemento carrossel para manipulação
+const carrossel = document.querySelector("#carrossel") 
+
+//selecionando o elemento imagem dentro do carrossel
+const imagem = document.querySelector("#carrossel>img") 
+
+function trocaImagem() {
+
+if (){
+
+
+
+}
+
+}
+
+setInterval(() => {
+    imagem.src = imagensCarrossel[]
+}, 2000);
+
+
+
